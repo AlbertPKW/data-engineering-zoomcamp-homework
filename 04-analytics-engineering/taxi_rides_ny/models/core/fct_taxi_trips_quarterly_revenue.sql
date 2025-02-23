503386b49b81c53cfb3ec8045120ccaea69202d4
poonkokweng@gmail.com <poonkokweng@gmail.com>
2025-02-23 20:43:56 +0000

    minor changes

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -31,15 +31,35 @@ rev_yoy as (
         LAG(revenue_year_quarter, 1) OVER
             (PARTITION BY service_type, revenue_quarter ORDER BY revenue_year) AS prev_year_rev
     from rev_year_quarter
+),
+yoy_calculated as (
+    select
+        service_type,
+        revenue_quarter,
+        revenue_year,
+        revenue_year_quarter,
+        prev_year_rev,
+        (revenue_year_quarter - prev_year_rev) / prev_year_rev * 100 as yoy_growth
+    from rev_yoy
+    where revenue_year = 2020 -- Focus only on 2020 YoY growth
+),
+yoy_ranking as (
+    select
+        service_type,
+        revenue_quarter,
+        yoy_growth,
+        -- Identify the best (max YoY growth) and worst (min YoY growth) quarters
+        CASE 
+            WHEN yoy_growth = (SELECT MAX(yoy_growth) FROM yoy_calculated y2 WHERE y2.service_type = y1.service_type) 
+            THEN 'Best' 
+        END as best_quarter,
+        CASE 
+            WHEN yoy_growth = (SELECT MIN(yoy_growth) FROM yoy_calculated y2 WHERE y2.service_type = y1.service_type) 
+            THEN 'Worst' 
+        END as worst_quarter
+    from yoy_calculated y1
 )
 
-select
-    service_type,
-    revenue_quarter,
-    revenue_year,
-    revenue_year_quarter,
-    prev_year_rev,
-    (revenue_year_quarter - prev_year_rev) / prev_year_rev * 100 as yoy_growth
-from rev_yoy
-order by 1,2,3 desc
+select * from yoy_ranking
+order by service_type, yoy_growth desc
 
